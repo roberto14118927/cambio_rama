2144d509c2698509ab54e4cac0ca405c407fa517
roberto14118927 <robertoruiz141189@gmail.com>
2019-06-07 14:25:59 -0500

    add

--- CS8/ubuntu_settings.py
+++ CS8/ubuntu_settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c7d5jvhbk%sdyt=vllu0=aw*&uu^)2&$ez0a88g)(-0=9v)y9l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['134.209.76.81']
 
 
 # Application definition
